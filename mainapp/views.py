ad31a8e0016637f9125daa99b8e8bd0cb6bd9ebe
Harsh <harshrawat096@gmail.com>
2023-07-15 10:49:54 +0530

    Add Yahoo-Finance Python Api Which use webscraping

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from yahoo_fin.stock_info import *
 
 # Create your views here.
 def stockPicker(request):
-    return render(request,'mainapp/stockpicker.html')
+    stock_picker = tickers_nifty50()
+    print(stock_picker)
+
+    #Context The Data
+    context = {'stockpicker':stock_picker}
+    return render(request,'mainapp/stockpicker.html',context)
 
 def stockTracker(request):
     return render(request,'mainapp/stocktracker.html')
