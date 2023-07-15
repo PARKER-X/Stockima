f1c51943c0b777a9fb324431c49fb17f0c19b2b5
Harsh <harshrawat096@gmail.com>
2023-07-15 12:22:14 +0530

    Update Api

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from yahoo_fin.stock_info import *
+from nsepython import *
 
 # Create your views here.
 def stockPicker(request):
@@ -11,4 +11,11 @@ def stockPicker(request):
     return render(request,'mainapp/stockpicker.html',context)
 
 def stockTracker(request):
+    n = NSELive()
+    details = n.stock_quote("HDFC")
+    
+    print(details)
+
+    # Context The Data
+    
     return render(request,'mainapp/stocktracker.html')
